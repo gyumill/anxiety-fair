e7937cb325540c6d59e2f3760849e6c1773bf537
gyumill <milacrobate@gmail,com>
2024-09-22 10:48:47 +0200

    padding

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -8,11 +8,11 @@ export default function MainPage() {
   const cardList: Card[] = cardsData;
 
   return (
-    <div className="w-full h-full max-w-screen-2xl p-8">
+    <div className="w-full h-full max-w-screen-2xl p-6 md:p-8">
       <h1 className="text-3xl md:text-5xl text-center pb-14">
         beležnica strašnih misli
       </h1>
-      <div className="grid grid-flow-row grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 p-10">
+      <div className="grid grid-flow-row grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 md:p-10">
         {cardList?.map((card) => (
           <FrameComponent
             onClick={() => navigate(`/card/${card.id}`)}
